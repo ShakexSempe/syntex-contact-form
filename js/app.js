3d94c03d793122e2b0a8b22f59c794068b070254
Ray-Shakex <bshakex@gmail.com>
2022-01-02 13:16:27 +0200

    first commit: setup

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// date
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
+// hamburger
+const toggle = document.getElementById("toggle");
+toggle.addEventListener("click", () => {
+    let navList = document.querySelector('.nav-list');
+    navList.classList.toggle("active");
+})
