dbfa45876f7f18a0a4bc3ab3381e2e44cb6af83f
Ray-Shakex <bshakex@gmail.com>
2022-01-10 12:09:18 +0200

    value-proposition div and js active logic

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ const navList = document.querySelector('.nav-list');
 const date = document.getElementById("date");
 const toggle = document.getElementById("toggle");
 const navItem = document.querySelectorAll(".nav-list .nav-item a");
+const infoBtn = document.querySelectorAll(".info-btn");
+const infoSection = document.querySelector('.info-section');
+
 // date
 date.innerHTML = new Date().getFullYear();
 
@@ -19,3 +22,10 @@ navItem.forEach(item => {
         toggle.classList.toggle("active");
     })
 });
+
+// info btn 
+infoBtn.forEach(btn => {
+    btn.addEventListener("click", () => {
+        infoSection.classList.toggle("active");
+    })
+})
