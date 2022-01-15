aac255925a1c2feebe49dd9297df9bbd811b05fe
Ray-Shakex <bshakex@gmail.com>
2022-01-15 10:33:02 +0200

    add features setup: js and scss

--- js/features.js
+++ js/features.js
@@ -1,6 +1,7 @@
 // VARIABLES
 const form = document.getElementById("form");
 const feature = document.getElementById("feature");
+const featureDescription = document.getElementById("feature-description");
 const container = document.querySelector(".feature-container");
 const list = document.querySelector("feature-list");
 const submitBtn = document.querySelector('.submit-btn');
@@ -12,6 +13,7 @@ let editID = '';
 
 console.log(form);
 console.log(feature);
+console.log(featureDescription);
 console.log(container);
 console.log(submitBtn);
 console.log(clearBtn);
