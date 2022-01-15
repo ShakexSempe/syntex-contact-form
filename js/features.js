a64418a3feda3d89ab25761f2217d23769258d0f
Ray-Shakex <bshakex@gmail.com>
2022-01-15 11:40:17 +0200

    addItem()

--- js/features.js
+++ js/features.js
@@ -1,9 +1,9 @@
 // VARIABLES
-const form = document.getElementById("form");
+const form = document.getElementById("features");
 const feature = document.getElementById("feature");
 const featureDescription = document.getElementById("feature-description");
 const container = document.querySelector(".feature-container");
-const list = document.querySelector("feature-list");
+const list = document.querySelector(".feature-list");
 const submitBtn = document.querySelector('.submit-btn');
 const clearBtn = document.querySelector('.clear-btn');
 
@@ -13,7 +13,51 @@ let editID = '';
 
 console.log(form);
 console.log(feature);
+console.log(list);
 console.log(featureDescription);
 console.log(container);
 console.log(submitBtn);
 console.log(clearBtn);
+
+const addItem = (e) => {
+    console.log(feature.value);
+    console.log(featureDescription.value);
+    const nameValue = feature.value;
+    const descValue = featureDescription.value;
+    const id = new Date().getTime().toString();
+
+    if(nameValue && !editFlag) {
+        const element = document.createElement('article');
+
+        element.classList.add('input-div');
+        // add id
+        const attribute = document.createAttribute('data-id');
+        attribute.value = id;
+        element.setAttributeNode(attribute);
+
+        element.innerHTML = `<label for="item-1">feature</label>
+        <input 
+            name="item-1"
+            id="item-1" 
+            title="Item 1" 
+            class="item-name" 
+            type="text" 
+            tabindex="40"
+            minlength="1" 
+            value="${nameValue}"
+            placeholder="${nameValue}" 
+            required>
+
+        <label for="item-name">Description</label>
+        <textarea name="item-description" id="item-1-description" title="Item description" cols="30" rows="5"
+            tabindex="41" 
+            minlength="1" 
+            value="${descValue}"
+            placeholder="${descValue}" 
+            required></textarea>`;
+
+        list.appendChild(element);
+    }
+}
+
+submitBtn.addEventListener('click' , addItem);
